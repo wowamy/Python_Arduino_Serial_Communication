428cede8cd07e77cb9530b1725c16d478eafba2a
wowamy <amy@virtuosorobotics.com>
2020-01-06 14:56:22 +0800

    Arduino

--- serialtesttest/serialtesttest.ino
+++ serialtesttest/serialtesttest.ino
@@ -1,19 +1,22 @@
-void setup()
-{
-  pinMode(13,OUTPUT);
+#define LED 13
+String str;
+
+void setup() {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop()
-{
-  if(Serial.available())
-  {
-    switch(Serial.read())
-    {
-      case '1':digitalWrite(13,HIGH);
-               break;
-      default: digitalWrite(13,LOW);
-               break;
+void loop() {
+  if (Serial.available()) {
+    // 讀取傳入的字串直到"\n"結尾
+    str = Serial.readStringUntil('\n');
+
+    if (str == "LED_ON") {           
+        digitalWrite(LED, HIGH);     
+        Serial.println("LED is ON");
+    } else if (str == "LED_OFF") {
+        digitalWrite(LED, LOW);
+        Serial.println("LED is OFF");
     }
   }
 }
